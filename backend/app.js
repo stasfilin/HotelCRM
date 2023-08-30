40b5c55d978fd2168c0deca5c8839795835a3402
Stanislav Filin <stasfilin@hotmail.com>
2023-08-30 14:55:10 +0000

    Add graphql intergration
    Signed-off-by: Stanislav Filin <stasfilin@hotmail.com>

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var { graphqlHTTP } = require("express-graphql")
+var { buildSchema } = require("graphql")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
